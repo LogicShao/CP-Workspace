83b2db6e7e63b41e84c1ef2e35a5a07af1764db4
logicshao <1712207189@qq.com>
2024-09-22 12:29:07 +0800

    💩fix: add .vscode/ to gitignore

---
+++ src/abc371/fanta5y/f.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+#define multi_test 0
+
+using LL = long long;
+using PII = std::pair<int, int>;
+
+void solve() {}
+
+int main() {
+  std::cin.tie(nullptr)->sync_with_stdio(false);
+
+  int T = 1;
+  if constexpr (multi_test) {
+    std::cin >> T;
+  }
+
+  while (T--) {
+    solve();
+  }
+}
