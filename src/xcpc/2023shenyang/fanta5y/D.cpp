9311c1700a7007626bf7d2010e01375276075dc7
logicshao <1712207189@qq.com>
2024-10-25 19:12:46 +0800

    upd

--- src/xcpc/2023shenyang/fanta5y/D.cpp
+++ src/xcpc/2023shenyang/fanta5y/D.cpp
@@ -1,62 +1,247 @@
 #include <bits/stdc++.h>
 
 #define multi_test 0
-#define DEBUG 1
+#define DEBUG 0
+#define MADE_IN_HEAVEN
+
+#ifdef MADE_IN_HEAVEN
+#pragma GCC optimize(3)
+#pragma GCC target("avx")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("inline")
+#pragma GCC optimize("-fgcse")
+#pragma GCC optimize("-fgcse-lm")
+#pragma GCC optimize("-fipa-sra")
+#pragma GCC optimize("-ftree-pre")
+#pragma GCC optimize("-ftree-vrp")
+#pragma GCC optimize("-fpeephole2")
+#pragma GCC optimize("-ffast-math")
+#pragma GCC optimize("-fsched-spec")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC optimize("-falign-jumps")
+#pragma GCC optimize("-falign-loops")
+#pragma GCC optimize("-falign-labels")
+#pragma GCC optimize("-fdevirtualize")
+#pragma GCC optimize("-fcaller-saves")
+#pragma GCC optimize("-fcrossjumping")
+#pragma GCC optimize("-fthread-jumps")
+#pragma GCC optimize("-funroll-loops")
+#pragma GCC optimize("-fwhole-program")
+#pragma GCC optimize("-freorder-blocks")
+#pragma GCC optimize("-fschedule-insns")
+#pragma GCC optimize("inline-functions")
+#pragma GCC optimize("-ftree-tail-merge")
+#pragma GCC optimize("-fschedule-insns2")
+#pragma GCC optimize("-fstrict-aliasing")
+#pragma GCC optimize("-fstrict-overflow")
+#pragma GCC optimize("-falign-functions")
+#pragma GCC optimize("-fcse-skip-blocks")
+#pragma GCC optimize("-fcse-follow-jumps")
+#pragma GCC optimize("-fsched-interblock")
+#pragma GCC optimize("-fpartial-inlining")
+#pragma GCC optimize("no-stack-protector")
+#pragma GCC optimize("-freorder-functions")
+#pragma GCC optimize("-findirect-inlining")
+#pragma GCC optimize("-fhoist-adjacent-loads")
+#pragma GCC optimize("-frerun-cse-after-loop")
+#pragma GCC optimize("inline-small-functions")
+#pragma GCC optimize("-finline-small-functions")
+#pragma GCC optimize("-ftree-switch-conversion")
+#pragma GCC optimize("-foptimize-sibling-calls")
+#pragma GCC optimize("-fexpensive-optimizations")
+#pragma GCC optimize("-funsafe-loop-optimizations")
+#pragma GCC optimize("inline-functions-called-once")
+#pragma GCC optimize("-fdelete-null-pointer-checks")
+#pragma GCC optimize(2)
+#pragma GCC optimize(3)
+#pragma GCC target("avx")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("inline")
+#pragma GCC optimize("-fgcse")
+#pragma GCC optimize("-fgcse-lm")
+#pragma GCC optimize("-fipa-sra")
+#pragma GCC optimize("-ftree-pre")
+#pragma GCC optimize("-ftree-vrp")
+#pragma GCC optimize("-fpeephole2")
+#pragma GCC optimize("-ffast-math")
+#pragma GCC optimize("-fsched-spec")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC optimize("-falign-jumps")
+#pragma GCC optimize("-falign-loops")
+#pragma GCC optimize("-falign-labels")
+#pragma GCC optimize("-fdevirtualize")
+#pragma GCC optimize("-fcaller-saves")
+#pragma GCC optimize("-fcrossjumping")
+#pragma GCC optimize("-fthread-jumps")
+#pragma GCC optimize("-funroll-loops")
+#pragma GCC optimize("-fwhole-program")
+#pragma GCC optimize("-freorder-blocks")
+#pragma GCC optimize("-fschedule-insns")
+#pragma GCC optimize("inline-functions")
+#pragma GCC optimize("-ftree-tail-merge")
+#pragma GCC optimize("-fschedule-insns2")
+#pragma GCC optimize("-fstrict-aliasing")
+#pragma GCC optimize("-fstrict-overflow")
+#pragma GCC optimize("-falign-functions")
+#pragma GCC optimize("-fcse-skip-blocks")
+#pragma GCC optimize("-fcse-follow-jumps")
+#pragma GCC optimize("-fsched-interblock")
+#pragma GCC optimize("-fpartial-inlining")
+#pragma GCC optimize("no-stack-protector")
+#pragma GCC optimize("-freorder-functions")
+#pragma GCC optimize("-findirect-inlining")
+#pragma GCC optimize("-fhoist-adjacent-loads")
+#pragma GCC optimize("-frerun-cse-after-loop")
+#pragma GCC optimize("inline-small-functions")
+#pragma GCC optimize("-finline-small-functions")
+#pragma GCC optimize("-ftree-switch-conversion")
+#pragma GCC optimize("-foptimize-sibling-calls")
+#pragma GCC optimize("-fexpensive-optimizations")
+#pragma GCC optimize("-funsafe-loop-optimizations")
+#pragma GCC optimize("inline-functions-called-once")
+#pragma GCC optimize("-fdelete-null-pointer-checks")
+#pragma GCC optimize(2)
+#pragma GCC optimize(3)
+#pragma GCC target("avx")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("inline")
+#pragma GCC optimize("-fgcse")
+#pragma GCC optimize("-fgcse-lm")
+#pragma GCC optimize("-fipa-sra")
+#pragma GCC optimize("-ftree-pre")
+#pragma GCC optimize("-ftree-vrp")
+#pragma GCC optimize("-fpeephole2")
+#pragma GCC optimize("-ffast-math")
+#pragma GCC optimize("-fsched-spec")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC optimize("-falign-jumps")
+#pragma GCC optimize("-falign-loops")
+#pragma GCC optimize("-falign-labels")
+#pragma GCC optimize("-fdevirtualize")
+#pragma GCC optimize("-fcaller-saves")
+#pragma GCC optimize("-fcrossjumping")
+#pragma GCC optimize("-fthread-jumps")
+#pragma GCC optimize("-funroll-loops")
+#pragma GCC optimize("-fwhole-program")
+#pragma GCC optimize("-freorder-blocks")
+#pragma GCC optimize("-fschedule-insns")
+#pragma GCC optimize("inline-functions")
+#pragma GCC optimize("-ftree-tail-merge")
+#pragma GCC optimize("-fschedule-insns2")
+#pragma GCC optimize("-fstrict-aliasing")
+#pragma GCC optimize("-fstrict-overflow")
+#pragma GCC optimize("-falign-functions")
+#pragma GCC optimize("-fcse-skip-blocks")
+#pragma GCC optimize("-fcse-follow-jumps")
+#pragma GCC optimize("-fsched-interblock")
+#pragma GCC optimize("-fpartial-inlining")
+#pragma GCC optimize("no-stack-protector")
+#pragma GCC optimize("-freorder-functions")
+#pragma GCC optimize("-findirect-inlining")
+#pragma GCC optimize("-fhoist-adjacent-loads")
+#pragma GCC optimize("-frerun-cse-after-loop")
+#pragma GCC optimize("inline-small-functions")
+#pragma GCC optimize("-finline-small-functions")
+#pragma GCC optimize("-ftree-switch-conversion")
+#pragma GCC optimize("-foptimize-sibling-calls")
+#pragma GCC optimize("-fexpensive-optimizations")
+#pragma GCC optimize("-funsafe-loop-optimizations")
+#pragma GCC optimize("inline-functions-called-once")
+#pragma GCC optimize("-fdelete-null-pointer-checks")
+#pragma GCC optimize(2)
+#endif
 
 using LL = long long;
 using PII = std::pair<int, int>;
 using ULL = unsigned long long;
 
 constexpr int N = 5e3 + 10, base = 1331;
-char s[N], t[N];
 ULL p[N];
-
+char s[N], t[N];
+int f[N][N];
+LL g[N][N];
+bool added;
 struct hash_handler {
   ULL h[N];
-  void init(const char *s) {
+
+  void init(char *s) {
     h[0] = 0;
-    for (int i = 1; s[i]; ++ i) {
-      h[i] = h[i - 1] * base + s[i];
+    for (int i = 1; s[i - 1]; ++i) {
+      h[i] = h[i - 1] * base + s[i - 1];
     }
   }
-  ULL get(int l, int r) {
-    return h[r] - h[l - 1] * p[r - l + 1];
-  }
+
+  ULL get(int l, int r) { return h[r] - h[l - 1] * p[r - l + 1]; }
 } sh, th;
 
 void pre_work() {
   p[0] = 1;
-  for (int i = 1; i < N; ++ i) {
+  for (int i = 1; i < N; ++i) {
     p[i] = p[i - 1] * base;
   }
 }
 
-void solve() {
-  std::cin >> (s + 1) >> (t + 1);
-  int n = strlen(s + 1), m = strlen(t + 1);
+LL solve(char *s, char *t, int n, int m) {
   sh.init(s);
   th.init(t);
-  std::map<ULL, int> cnts, cntt;
-  for (int i = 1; i <= n; ++ i) {
-    for (int j = i; j <= n; ++ j) {
-      cnts[sh.get(i, j)] ++;
+
+  std::unordered_map<ULL, int> cntt;
+  for (int i = 1; i <= m; ++i) {
+    for (int j = i; j <= m; ++j) {
+      cntt[th.get(i, j)]++;
     }
   }
-  for (int i = 1; i <= m; ++ i) {
-    for (int j = i; j <= m; ++ j) {
-      cntt[th.get(i, j)] ++;
+
+  LL ans = 0;
+  if (not added) {
+    for (int i = 1; i <= n; ++i) {
+      for (int j = i; j <= n; ++j) {
+        auto h = sh.get(i, j);
+        if (cntt.count(h)) {
+          ans += cntt[h];
+        }
+      }
+    }
+    added = true;
+  }
+
+  for (int i = n; i >= 1; --i) {
+    for (int j = n; j >= i; --j) {
+      if (s[i - 1] == s[j - 1]) {
+        f[i][j] = i + 1 <= n and j + 1 <= n ? f[i + 1][j + 1] + 1 : 1;
+      } else {
+        f[i][j] = 0;
+      }
     }
   }
-  LL res = 0;
-  for (auto [k, v] : cnts) {
-    if (not cntt.count(k)) continue;
-    res += 1LL * v * cntt[k];
+
+  // g[i][j] 表示 s[i,j] 的所有后缀在 t 中出现的次数
+  for (int j = n; j >= 1; --j) {
+    for (int i = j; i >= 1; --i) {
+      if (i == j) {
+        g[i][j] = cntt[sh.get(i, j)];
+      } else {
+        g[i][j] = g[i + 1][j] + cntt[sh.get(i, j)];
+      }
+    }
   }
-  std::cout << res << '\n';
+
+  for (int i = 1; i <= n; ++i) {
+    for (int j = i + 2; j <= n; ++j) {
+      if (not f[i][j])
+        continue;
+      int pos = i + f[i][j];
+      ans += g[i + 1][j - 1];
+      if (pos + 1 < j) {
+        ans -= g[pos + 1][j - 1];
+      }
+    }
+  }
+
+  return ans;
 }
 
 int main() {
-  std::cin.tie(nullptr)->sync_with_stdio(false);
   if constexpr (DEBUG) {
     std::cerr.tie(nullptr);
     freopen("data.in", "r", stdin);
@@ -64,12 +249,14 @@ int main() {
 
   pre_work();
 
-  int T = 1;
-  if constexpr (multi_test) {
-    std::cin >> T;
-  }
+  scanf("%s%s", s, t);
 
-  while (T--) {
-    solve();
-  }
+  int n = strlen(s), m = strlen(t);
+  auto res = solve(s, t, n, m);
+
+  std::reverse(s, s + n);
+  std::reverse(t, t + m);
+
+  res += solve(t, s, m, n);
+  printf("%lld\n", res);
 }
--- src/xcpc/2023shenyang/fanta5y/K.cpp
+++ src/xcpc/2023shenyang/fanta5y/K.cpp
@@ -8,8 +8,8 @@ using PII = std::pair<int, int>;
 
 struct SegmentTree {
   struct node {
-    int l, r;
-    LL sum, add;
+    int l, r, cnt;
+    LL sum;
   };
 
 #define lson (u << 1)
@@ -19,49 +19,69 @@ struct SegmentTree {
 #define pr tr[rson]
 
   std::vector<node> tr;
-  SegmentTree(int n) : tr((n + 5) << 2) {}
+  SegmentTree(int n) : tr((n + 5) << 2) { build(1, 1, n); }
 
-  void push_up(int u) { p.sum = pl.sum + pr.sum; }
-
-  void push_down(int u) {
-    if (p.add) {
-      pl.sum += p.add * (pl.r - pl.l + 1);
-      pr.sum += p.add * (pr.r - pr.l + 1);
-      pl.add += p.add;
-      pr.add += p.add;
-      p.add = 0;
-    }
-  }
-
-  void modify(int u, int l, int r, int d) {
-    if (p.l >= l and p.r <= r) {
-      p.sum += d * (p.r - p.l + 1);
-      p.add += d;
+  void build(int u, int l, int r) {
+    p.l = l, p.r = r;
+    p.sum = p.cnt = 0;
+    if (l == r) {
       return;
     }
-    push_down(u);
-    int mid = (p.l + p.r) >> 1;
-    if (l <= mid) {
-      modify(lson, l, r, d);
+    int mid = (l + r) >> 1;
+    build(lson, l, mid);
+    build(rson, mid + 1, r);
+  }
+
+  void push_up(int u) {
+    p.sum = pl.sum + pr.sum;
+    p.cnt = pl.cnt + pr.cnt;
+  }
+
+  void modify(int u, int x, int v, int t) {
+    // x 位置加上 t 个 v
+    if (p.l == p.r) {
+      p.sum += 1ll * t * v;
+      p.cnt += t;
+      return;
     }
-    if (r > mid) {
-      modify(rson, l, r, d);
+    int mid = (p.l + p.r) >> 1;
+    if (x <= mid) {
+      modify(lson, x, v, t);
+    } else {
+      modify(rson, x, v, t);
     }
     push_up(u);
   }
 
-  LL query(int u, int l, int r) {
-    if (p.l >= l and p.r <= r) {
+  LL query_sum(int u, int l, int r) {
+    if (l > r) {
+      return 0;
+    }
+    if (l <= p.l && p.r <= r) {
       return p.sum;
     }
-    push_down(u);
     int mid = (p.l + p.r) >> 1;
     LL res = 0;
     if (l <= mid) {
-      res += query(lson, l, r);
+      res += query_sum(lson, l, r);
     }
     if (r > mid) {
-      res += query(rson, l, r);
+      res += query_sum(rson, l, r);
+    }
+    return res;
+  }
+
+  int query_cnt(int u, int l, int r) {
+    if (l <= p.l && p.r <= r) {
+      return p.cnt;
+    }
+    int mid = (p.l + p.r) >> 1;
+    int res = 0;
+    if (l <= mid) {
+      res += query_cnt(lson, l, r);
+    }
+    if (r > mid) {
+      res += query_cnt(rson, l, r);
     }
     return res;
   }
@@ -76,41 +96,80 @@ struct SegmentTree {
 void solve() {
   int n, q;
   std::cin >> n >> q;
+
   std::vector<int> postive(1), a(n + 1);
+  LL negtivesum = 0;
+
   for (int i = 1; i <= n; ++i) {
     std::cin >> a[i];
+
     if (a[i] > 0) {
-      postive.push_back(i);
+      postive.push_back(a[i]);
+    } else {
+      negtivesum += a[i];
     }
   }
+
   std::vector<PII> change(q);
   for (auto &[x, v] : change) {
     std::cin >> x >> v;
     if (v > 0) {
-      postive.push_back(x);
+      postive.push_back(v);
     }
   }
+
   std::sort(postive.begin() + 1, postive.end());
   postive.erase(std::unique(postive.begin() + 1, postive.end()), postive.end());
+
   std::map<int, int> rnk;
   for (int i = 1; i < (int)postive.size(); ++i) {
     rnk[postive[i]] = i;
   }
+
   SegmentTree st(postive.size());
   for (int i = 1; i <= n; ++i) {
     if (a[i] > 0) {
-      st.modify(1, rnk[i], rnk[i], a[i]);
+      st.modify(1, rnk[a[i]], a[i], 1);
     }
   }
+
   for (auto &[x, v] : change) {
-    
+    if (a[x] > 0) {
+      st.modify(1, rnk[a[x]], a[x], -1);
+    } else {
+      negtivesum -= a[x];
+    }
+
+    a[x] = v;
+
+    if (a[x] > 0) {
+      st.modify(1, rnk[a[x]], a[x], 1);
+    } else {
+      negtivesum += a[x];
+    }
+
+    int l = 0, r = postive.size() - 1;
+    while (l < r) {
+      int mid = (l + r + 1) >> 1;
+      if (st.query_sum(1, 1, mid) + negtivesum <= 0) {
+        l = mid;
+      } else {
+        r = mid - 1;
+      }
+    }
+
+    LL rest = -(st.query_sum(1, 1, l) + negtivesum);
+    int k = st.query_cnt(1, l + 1, l + 1) and l + 1 < (int)postive.size()
+                ? st.query_cnt(1, 1, l) + rest / postive[l + 1]
+                : st.query_cnt(1, 1, l);
+    std::cout << k + 1 << '\n';
   }
 }
 
 int main() {
   std::cin.tie(nullptr)->sync_with_stdio(false);
   if constexpr (DEBUG) {
-    std::cerr.tie(nullptr)->sync_with_stdio(false);
+    std::cerr.tie(nullptr);
     freopen("data.in", "r", stdin);
   }
 
